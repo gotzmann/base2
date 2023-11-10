81c8b34a7ebe07364b51ac9b05268e3d2a7580f7
Serge Gotsuliak <sgotsulyak@gmail.com>
2023-11-10 23:15:37 +0300

    ...

--- team_code/generate.py
+++ team_code/generate.py
@@ -412,7 +412,7 @@ def get_ppl(model, tokenizer, cur_query_tuple, history_tensor=None):
         predictions = data, 
         model_id = "/app/mistral", #  MODEL,
         # cache_dir = "/app/models",
-        device = DEVICE,
+        #device = DEVICE,
     )
 
     print("\n=== RESULT ===\n", result)
